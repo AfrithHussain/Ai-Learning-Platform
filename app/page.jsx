7249fb612065ea1d2a86887de41561a338e43d15
AfrithHussain <afrithhussain64@gmail.com>
2025-07-23 17:05:32 +0530

    pproject setup complete

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+function Home() {
+  return (
+    <div>
+        
+      <h1 className=''>hello world</h1>
+      
+    </div>
+  )
+}
+
+export default Home;
